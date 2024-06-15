2c0a0515c54c6f59768505ccac58e6a708e95df5
miceremwirigi <miceremwirigi@gmail.com>
2024-06-16 00:58:19 +0300

    fixed post cyber to use absolute url

--- src/Components/AddCyberForm.js
+++ src/Components/AddCyberForm.js
@@ -26,7 +26,7 @@ class AddCyberForm extends Component {
         entry.preventDefault();
         console.log(entry)
         this.setState({submitted:true})
-        fetch("/apis/cybers",{
+        fetch("https://easybima-backend.onrender.com/apis/cybers/",{
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
